cbbf9e27f74bd87b7b51754dc5e38080dc59a33f
seb <104381156+slhuilli1@users.noreply.github.com>
2023-06-24 07:17:05 +0200

    Define
    This plugin will show a list or words ans their explanation. All are stored in custom field.
    More explanation : https://www.sebastien-lhuillier.com/index.php/informatique/joomla-12/plugins-joomla/842-afficher-des-definitions

---
+++ definitions.php
@@ -0,0 +1,48 @@
+<?php
+	defined('_JEXEC') or die('Access deny');
+
+	class plgContentDefinitions extends JPlugin 
+	{
+		function onContentPrepare($content, $article, $params, $limit){	
+		
+					$re = '{def}';
+					
+					$doc = JFactory::getDocument();
+
+					$doc->addStyleSheet('plugins/content/definitions/style.css');
+
+
+					$i=0;
+					JLoader::register('FieldsHelper', JPATH_ADMINISTRATOR . '/components/com_fields/helpers/fields.php');
+					$customFields = FieldsHelper::getFields('com_users.user', JFactory::getUser(), true);
+					$a = FieldsHelper::getFields('com_content.article', $article);
+
+					$t=array();
+					$t = (array)json_decode($a[2]->value);
+					$chaine = '<div class="definitions"><div class="titre">Définitions en lien avec le matériel</div>';
+					foreach ($t as $maligne){
+						/*$chaine .='<hr><div class="une-ligne-definition">';
+						
+						$chaine .= '<div class="un-acronyme" id="un-acronyme-'.$i.'">'.$maligne->def_acronyme."</div>";
+						$chaine .= '<div class="un-mot" id="un-mot-'.$i.'">'.$maligne->def_mot."</div>";
+						$chaine .= '<div class="une-definition" id="une-definition-'.$i.'">'.$maligne->def_definition."</div>";
+						$chaine .= '<div class="une-note" id="une-note-'.$i.'">'.$maligne->def_notes."</div>";
+						
+						$chaine .='</div></hr>';*/
+						$chaine .= '<details>';
+						$chaine .= '<summary>'.$maligne->def_mot.'<acronym>'.$maligne->def_acronyme.'</acronym></summary>';
+						$chaine .= '<p>';
+						$chaine .= $maligne->def_definition;
+						$chaine .= '</p>';
+						$chaine .= '</details>';
+					}
+					$chaine .='</div>';
+					$i++;
+					$article->text = str_replace($re, $chaine,  $article->text);
+				}
+			
+	}
+
+
+	
+
